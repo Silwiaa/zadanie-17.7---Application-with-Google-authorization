87eb5444443dcc0195d667d21a2a888daf92ef68
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-07-18 20:47:46 +0200

    create new repository

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE-CLIENT_ID': '1008043405880-uua2t4pttkeuqmmiq21a9kqnnv3kffhv.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'SjeSzELuDyguUjst0NY4TY47',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
